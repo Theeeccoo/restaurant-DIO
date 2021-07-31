bf6e407198994f537f916df1bf11e1ba4ee466ab
Thiago Nolasco <thiagodecampos1@gmail.com>
2021-07-31 17:08:10 -0300

    ADD homepage && reset styles

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Reset } from 'styled-reset';
 
-import Home from './pages/Home';
 import theme from './theme';
+import Home from './pages/Home';
 
-function App() {
-  return (
+function App(){
+  return(
     <ThemeProvider theme={theme}>
       <Reset />
       <Home />
     </ThemeProvider>
   );
 }
+
 
 export default App;
